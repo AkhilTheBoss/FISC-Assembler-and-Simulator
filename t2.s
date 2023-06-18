4e87bd7a4c19ad264f6c9d6c2522b90c8b5c2739
Akhileshwar Shriram <112577383+AkhilTheBoss@users.noreply.github.com>
2023-06-18 12:23:50 -0700

    Update t2.s

--- t2.s
+++ t2.s
@@ -4,8 +4,9 @@ start:  not r0 r1
         add r1 r1 r1
         not r1 r1
 loop:   and r3 r0 r0
-        add r0 r0 r1
-        bnz loop
+        add r0 r1 r3
+        bnz endloop   ; New line: Break out of the loop if r0 is not zero
         and r3 r3 r3
         not r2 r3
         and r1 r2 r3
+endloop: add r0 r0 r0
